d378e871a4f8777de66ee366c3ed3710913e05b8
oussama hmida haj boubaker rais <147174486+oussemahajboubaker@users.noreply.github.com>
2024-07-02 16:20:42 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  const mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function(event) { event.preventDefault(); mobileNavToogle();})
+  });
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+
+  new PureCounter();
+
+  function aos_init() {
+    AOS.init({
+      duration: 800,
+      easing: 'slide',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+});
